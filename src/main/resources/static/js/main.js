c8d67bc18a5f600878cd9300dae445481dd4c42a
Brice Blanch <bricerb@gmail.com>
2017-06-12 09:27:57 -0400

    Lot of work on the list page, no posts setup yet, functionality for todo list is here, some simple styling done

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,51 @@
-let input = document.querySelector("#input")
-let userName = document.querySelector(".user-name")
+let input = document.querySelector("#input");
+let inputV = input.value;
+let userName = document.querySelector(".user-name");
+let todoList = document.querySelector(".list");
+let listContainer = document.querySelector(".list-container");
+let icon = document.querySelector(".icon");
 let user = {
-  "name": "derp"
-}
+  name: "harro",
+};
+let isHiddenList = false;
+let listData = [];
 
 let setUserName = function() {
   userName.innerHTML = user.name;
+}();
+
+let setList = function(str) {
+  let text = "<li>" + str + "</li>";
+  todoList.insertAdjacentHTML("beforeend", text);
+};
+
+let addItem = function() {
+  if (isHiddenList) {
+    listContainer.classList = "list-container";
+  }
+  inputV = input.value;
+  setList(inputV);
+  input.value = "";
+};
+
+icon.addEventListener("click", function(e) {
+  addItem();
+});
+
+input.addEventListener("keypress", function(e) {
+  if (e.keyCode === 13) {
+    addItem();
+  }
+});
+
+if (listData.length === 0) {
+  listContainer.classList = "list-hide";
+  isHiddenList = true;
+} else {
+  for (var i = 0; i < listData.length; i++) {
+    setList(listData[i]);
+  }
 }
-// console.log("name", user.name);
-// console.log("name2", user.name2);
-// console.log("names", user.names);
-
-setUserName();
 
 
 
@@ -20,12 +54,19 @@ setUserName();
 
 
 
-
-
-
-
-
-
+// $scope.setCurrentScaleLevel = function(scaleLevel) {
+//         console.log("Setting scale level for user");
+//         $scope.user.currentScaleLevel = scaleLevel;
+//         $http.post("/getDesiredScaleLevel.json", $scope.user)
+//         .then (
+//             function successCallBack(response) {
+//                 console.log("User updated");
+//                 $scope.currentScaleLevel = response.data;
+//             },
+//             function errorCallBack(response) {
+//                 console.log("Unable to update user");
+//             });
+//     };
 
 
 
